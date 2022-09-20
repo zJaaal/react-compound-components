c92c50c2485ec9e80bb98345b4c77609ef57bbe5
Jalinson Diaz <zjaaaldev@gmail.com>
2022-09-19 21:12:51 -0300

    Update TextButton.jsx

--- src/components/TextButton.jsx
+++ src/components/TextButton.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const TextButton = ({ random, setRandom }) => {
   return (
     <div>
-      {" "}
       <p style={{ color: random > 80 ? "green" : "violet" }}>I'm {random}</p>
       <button onClick={() => setRandom(0)}>Reset</button>
     </div>
